00c80cb2f55a420a5b5f3f8b6501ce9e0d96e9c2
Fadi Ft Ftena <fadiftenaft@gmail.com>
2024-03-28 01:50:19 +0300

    feature: add authentication module

--- challenge 2/salesms/src/main/java/maids/quiz/salesms/auth/RegisterRequest.java
+++ challenge 2/salesms/src/main/java/maids/quiz/salesms/auth/RegisterRequest.java
@@ -1,20 +1,19 @@
 package maids.quiz.salesms.auth;
 
-import com.alibou.security.user.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import maids.quiz.salesms.user.Role;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterRequest {
-
-  private String firstname;
-  private String lastname;
-  private String email;
-  private String password;
-  private Role role;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private Role role;
 }
