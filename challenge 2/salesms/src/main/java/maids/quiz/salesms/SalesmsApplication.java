e0ea3b5ac13ced7b10862d1c3702e72ccf0f5f27
Fadi Ft Ftena <fadiftenaft@gmail.com>
2024-03-28 01:50:58 +0300

    feature: commandline runner - create admin user when the application starts

--- challenge 2/salesms/src/main/java/maids/quiz/salesms/SalesmsApplication.java
+++ challenge 2/salesms/src/main/java/maids/quiz/salesms/SalesmsApplication.java
@@ -1,13 +1,34 @@
 package maids.quiz.salesms;
 
+import maids.quiz.salesms.auth.AuthenticationService;
+import maids.quiz.salesms.auth.RegisterRequest;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import static maids.quiz.salesms.user.Role.ADMIN;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class SalesmsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SalesmsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SalesmsApplication.class, args);
+    }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(AuthenticationService service) {
+        return args -> {
+            var admin = RegisterRequest.builder()
+                    .firstname("Admin")
+                    .lastname("Admin")
+                    .email("admin@mail.com")
+                    .password("password")
+                    .role(ADMIN)
+                    .build();
+            System.out.println("Admin token: " + service.register(admin).getAccessToken());
+        };
+    }
 }
