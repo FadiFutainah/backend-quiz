56c7bdefda7966fc0c2298379bcfc5e8e0f0f935
Fadi Ft Ftena <fadiftenaft@gmail.com>
2024-03-27 15:45:43 +0300

    feature: initialize challenge 2 project structure

---
+++ challenge 2/dev/src/test/java/maids/quiz/salesms/SalesmsApplicationTests.java
@@ -0,0 +1,13 @@
+package maids.quiz.salesms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
